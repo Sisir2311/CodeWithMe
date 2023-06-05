1ca72c4ebfe2d3f75d2491f4562262d8f6588ca6
Sisir2311 <74948767+Sisir2311@users.noreply.github.com>
2023-06-05 22:01:14 +0530

    Create Solution.java

---
+++ juspay_time/ques5/Solution.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int maxDiameter = 0;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        calculateHeight(root);
+        return maxDiameter;
+    }
+
+    private int calculateHeight(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+
+        int leftHeight = calculateHeight(node.left);
+        int rightHeight = calculateHeight(node.right);
+
+        // Calculate the diameter passing through the current node
+        int diameter = leftHeight + rightHeight;
+
+        // Update the maxDiameter if the current diameter is larger
+        if (diameter > maxDiameter) {
+            maxDiameter = diameter;
+        }
+
+        // Return the height of the current node
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+}
